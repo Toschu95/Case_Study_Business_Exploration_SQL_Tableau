ce6d8ab66702ac89ef3a448655369ddde37197d8
Toni S. <89910378+Toschu95@users.noreply.github.com>
2025-02-19 11:02:04 +0100

    Add SQL

---
+++ 10_SQL/sql_queries/01_schema_setup.sql
@@ -0,0 +1,7 @@
+-- Select database
+-- USE magist;
+
+-- Create indexes for performance optimization (idx already included in schema)
+-- CREATE INDEX idx_products_category ON products(product_category_name);
+-- CREATE INDEX idx_order_items_product ON order_items(product_id);
+-- CREATE INDEX idx_orders_order_id ON orders(order_id);

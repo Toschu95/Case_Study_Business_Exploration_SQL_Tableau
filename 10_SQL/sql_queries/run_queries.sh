00c50db56cbd6dc41b808e2a9681c5ff6a3ad20a
Toni S. <89910378+Toschu95@users.noreply.github.com>
2025-02-19 11:13:23 +0100

    Delete 10_SQL/sql_queries directory

--- 10_SQL/sql_queries/run_queries.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mysql -u root -p magist < 01_schema_setup.sql
-mysql -u root -p magist < 02_views_and_procedures.sql
-mysql -u root -p magist < 03_data_aggregation.sql
-mysql -u root -p magist < 04_analytics_queries.sql
